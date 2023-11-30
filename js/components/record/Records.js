c5977e1d05222b97993032c3a3690710db6c817c
Max Chopart <max.chopart@gmail.com>
2023-11-30 14:43:55 +0100

    [New #18] Added Publish button for Admin Operator

--- js/components/record/Records.js
+++ js/components/record/Records.js
@@ -1,13 +1,16 @@
+'use strict';
+
 import React from "react";
 import {Button, Card} from "react-bootstrap";
 import {injectIntl} from "react-intl";
 import withI18n from "../../i18n/withI18n";
 import RecordTable from "./RecordTable";
-import {ACTION_STATUS, ALERT_TYPES, ROLE} from "../../constants/DefaultConstants";
+import {ACTION_STATUS, ALERT_TYPES, EXTENSION_CONSTANTS, ROLE} from "../../constants/DefaultConstants";
 import AlertMessage from "../AlertMessage";
 import {LoaderSmall} from "../Loader";
 import PropTypes from "prop-types";
 import {processTypeaheadOptions} from "./TypeaheadAnswer";
+import {EXTENSIONS} from "../../../config";
 import ExportRecordsDropdown from "./ExportRecordsDropdown";
 
 const STUDY_CLOSED_FOR_ADDITION = false;
@@ -35,6 +38,9 @@ class Records extends React.Component {
         const showCreateButton = STUDY_CREATE_AT_MOST_ONE_RECORD
             ? (!recordsLoaded.records || (recordsLoaded.records.length < 1))
             : true;
+        const showPublishButton =
+            this.props.currentUser.role === ROLE.ADMIN
+            && EXTENSIONS === EXTENSION_CONSTANTS.OPERATOR;
         const createRecordDisabled =
             STUDY_CLOSED_FOR_ADDITION
             && (!this._isAdmin());
@@ -54,10 +60,13 @@ class Records extends React.Component {
                 <RecordTable {...this.props}/>
                 <div className="d-flex justify-content-between">
                     {showCreateButton
-                        ? <Button variant='primary' size='sm'
+                        ? <Button className="mx-1" variant='primary' size='sm'
                                   disabled={createRecordDisabled}
                                   title={createRecordTooltip}
                                   onClick={onCreateWithFormTemplate}>{this.i18n('records.create-tile')}</Button>
+                        : null}
+                    {showPublishButton ?
+                        <Button className="mx-1" variant='success' size='sm'>{this.i18n('publish')}</Button>
                         : null}
                     <ExportRecordsDropdown onExport={this.props.handlers.onExport} records={recordsLoaded.records}/>
                 </div>
