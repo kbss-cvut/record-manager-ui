045cce7a195f928fe751122a86a28808e00d2f37
shellyear <boolevafunkce@gmail.com>
2024-02-22 15:47:47 +0100

    chore: move files, and folders; add webpack rules; add public folder; include fonts in css;

--- webpack.config.js
+++ webpack.config.js
@@ -97,6 +97,26 @@ module.exports = (env = {}) => {
                         },
                     ],
                 },
+                {
+                    test: /\.(png|jpe?g|gif)$/i,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                        },
+                    ],
+                },
+                {
+                    test: /\.(woff|woff2|eot|ttf|otf)$/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                name: '[name].[ext]',
+                                outputPath: 'fonts/'
+                            }
+                        }
+                    ]
+                }
             ],
         },
         stats: {
@@ -111,7 +131,8 @@ module.exports = (env = {}) => {
                 version: env.production ? version : 'Dev',
                 year: new Date().getFullYear(),
                 title: appTitle,
-                template: 'index.html',
+                template: resolve(__dirname, 'public/index.html'),
+                favicon: resolve(__dirname, 'public/favicon.ico'),
                 inject: true,
                 minify: env.production,
                 basename: (isStatic || isForceBasename) ? basename : '',
@@ -144,7 +165,7 @@ module.exports = (env = {}) => {
             new CopyWebpackPlugin(
                 [
                     {
-                        from: resolve('./resources'),
+                        from: resolve('./public'),
                     },
                 ],
                 { copyUnmodified: true },
