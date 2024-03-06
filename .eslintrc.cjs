aece51bb45c07417ad24739b8a5f845d6f78f5ee
shellyear <boolevafunkce@gmail.com>
2024-03-06 17:38:24 +0100

    [chore] remove unused dependencies

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,13 @@
 module.exports = {
   parser: "@babel/eslint-parser",
-  extends: ["plugin:react/recommended"],
+  extends: ["plugin:react/recommended", "prettier"],
   parserOptions: {
     requireConfigFile: false,
     babelOptions: {
       presets: ["@babel/preset-react"],
     },
   },
-  plugins: ["react"],
+  plugins: ["react", "react-hooks"],
   rules: {}, // add specific rules here
   ignorePatterns: ["dist", "tests", ".eslintrc.cjs"],
   settings: {
