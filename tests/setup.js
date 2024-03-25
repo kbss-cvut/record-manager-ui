542cb6b75102e3fb2a8c324e6bfbf1461946b16d
shellyear <boolevafunkce@gmail.com>
2024-03-25 12:56:56 +0100

    [chore] remove jest, @cfaester/enzyme-adapter-react-18 and enzyme (with setups)

--- tests/setup.js
+++ tests/setup.js
@@ -1,13 +1,8 @@
-import enzyme, { shallow, mount } from "enzyme";
-import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
-
-enzyme.configure({ adapter: new Adapter() });
+import { test } from "vitest";
 
 require("dotenv-safe").config({
   allowEmptyValues: true,
   path: "./.env.test",
   sample: "./.env.example",
 });
-global.shallow = shallow;
-global.mount = mount;
 global.xit = test.skip;
