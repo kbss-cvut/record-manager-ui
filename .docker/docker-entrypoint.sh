8dbfdedf5ec16240c7e6770bce16ef1e9ac8cbb2
Miroslav Blasko <blcham@gmail.com>
2024-05-07 01:04:06 +0200

    Fix substitution in docker entrypoint

--- .docker/docker-entrypoint.sh
+++ .docker/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 set -eu
 
-envsubst '${API_URL} ${APP_TITLE} ${APP_INFO} ${LANGUAGE} ${NAVIGATOR_LANGUAGE} ${BASENAME} ${EXTENSIONS} ${AUTHENTICATION} ${AUTH_SERVER_URL} ${AUTH_CLIENT_ID}' < /etc/nginx/config.js.template > /var/www/config.js
+envsubst '${API_URL} ${APP_TITLE} ${APP_INFO} ${LANGUAGE} ${NAVIGATOR_LANGUAGE} ${BASENAME} ${EXTENSIONS} ${AUTHENTICATION} ${AUTH_SERVER_URL} ${AUTH_CLIENT_ID} ${ANALYTICS_URL}' < /etc/nginx/config.js.template > /var/www/config.js
 
 exec "$@"
