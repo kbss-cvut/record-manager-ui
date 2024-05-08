f6664005b85f50dbd1004915226f9f6fe83535b5
shellyear <boolevafunkce@gmail.com>
2024-05-09 00:35:01 +0200

    [feature] implement new pagination

--- src/components/misc/Pagination.jsx
+++ src/components/misc/Pagination.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import PropTypes from "prop-types";
+import PropTypes, { array } from "prop-types";
 import { Pagination as BSPagination } from "react-bootstrap";
 import { DEFAULT_PAGE_SIZE, STORAGE_TABLE_PAGE_SIZE_KEY } from "../../constants/DefaultConstants";
 import Input from "../Input";
@@ -10,6 +10,7 @@ import BrowserStorage from "../../utils/BrowserStorage";
  * The first page.
  */
 export const INITIAL_PAGE = 0;
+export const MIN_PAGINATION_NUMBER = 5;
 
 const PAGE_SIZES = [10, 20, 30, 50];
 const INFINITE_PAGE_SIZE = Math.pow(2, 31) - 1;
@@ -30,17 +31,56 @@ const Pagination = ({ pageNumber, handlePagination, itemCount, pageCount, allowS
     handlePagination(INITIAL_PAGE);
   };
 
+  const generatePageNumbers = () => {
+    let range = Array.from({ length: pageCount }, (v, i) => i);
+
+    if (pageCount < MIN_PAGINATION_NUMBER) {
+      // show all pages, without ellipsis
+      return range;
+    }
+
+    const pagesToShow = 4;
+    const pageNumbers = [];
+    let pageNumbersStart = pageNumber >= pageCount - pagesToShow ? pageCount - pagesToShow : pageNumber;
+    let pageNumbersEnd = Math.min(pageNumber + pagesToShow, pageCount);
+
+    pageNumbers.push(...range.slice(pageNumbersStart, pageNumbersEnd));
+
+    const isLeftEllipsis = pageNumber > INITIAL_PAGE;
+    const isRightEllipsis = pageNumber < pageCount / 2;
+
+    if (isLeftEllipsis) {
+      pageNumbers.splice(0, 0, "...");
+    }
+    if (isRightEllipsis) {
+      pageNumbers.splice(pageNumbers.length - 1, 1, "...");
+    }
+
+    return pageNumbers;
+  };
+
   return (
     <>
       <div className="d-flex justify-content-center">
         <BSPagination>
           <BSPagination.First disabled={pageNumber === INITIAL_PAGE} onClick={() => handlePagination(INITIAL_PAGE)} />
           <BSPagination.Prev disabled={pageNumber === INITIAL_PAGE} onClick={() => handlePagination(pageNumber - 1)} />
-          <BSPagination.Item active={true}>{pageNumber + 1}</BSPagination.Item>
-          <BSPagination.Next disabled={itemCount < pageSize} onClick={() => handlePagination(pageNumber + 1)} />
-          {pageCount !== undefined && pageNumber < pageCount && (
-            <BSPagination.Item onClick={() => handlePagination(pageCount)}>{pageCount + 1}</BSPagination.Item>
+          {generatePageNumbers().map((pageNum, i) => {
+            if (pageNum === "...") {
+              return <BSPagination.Ellipsis key={i} />;
+            }
+            return (
+              <BSPagination.Item onClick={() => handlePagination(pageNum)} active={pageNum === pageNumber} key={i}>
+                {pageNum + 1}
+              </BSPagination.Item>
+            );
+          })}
+          {pageCount !== undefined && (
+            <BSPagination.Item active={pageNumber === pageCount} onClick={() => handlePagination(pageCount)}>
+              {pageCount + 1}
+            </BSPagination.Item>
           )}
+          <BSPagination.Next disabled={pageNumber >= pageCount} onClick={() => handlePagination(pageNumber + 1)} />
         </BSPagination>
       </div>
       {allowSizeChange && (
