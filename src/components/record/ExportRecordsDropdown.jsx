9f8e3b06d9fe83efe76fb4d34b80634a33204d61
Max Chopart <chopart.max@proton.me>
2024-07-02 17:32:03 +0200

    [Upd] Added MOCK of Excel records export button

--- src/components/record/ExportRecordsDropdown.jsx
+++ src/components/record/ExportRecordsDropdown.jsx
@@ -18,8 +18,8 @@ const ExportRecordsDropdown = ({ records, onExport }) => {
       variant="primary"
       className="action-button"
     >
-      {/* Excel export is currently not supported by the backend
-        <Dropdown.Item onClick={() => onExport(ExportType.EXCEL)}>{i18n("records.export.excel")}</Dropdown.Item>*/}
+      {/*Excel export is currently not supported by the backend, uncommenting for demonstration purposes*/}
+      <Dropdown.Item onClick={() => onExport(ExportType.EXCEL)}>{i18n("records.export.excel")}</Dropdown.Item>
       <Dropdown.Item onClick={() => onExport(ExportType.JSON)}>{i18n("records.export.json")}</Dropdown.Item>
     </DropdownButton>
   );
