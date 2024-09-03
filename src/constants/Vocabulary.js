deba265e59f8a510df9f252958002e2aad7f2663
Daniil Palagin <daniilpalaginjob@gmail.com>
2024-09-03 20:56:44 +0200

    [#202] Add new types and roles

--- src/constants/Vocabulary.js
+++ src/constants/Vocabulary.js
@@ -8,3 +8,17 @@ export const RDFS_COMMENT = "http://www.w3.org/2000/01/rdf-schema#comment";
 export const ADMIN_TYPE = "http://onto.fel.cvut.cz/ontologies/record-manager/administrator";
 export const DOCTOR_TYPE = "http://onto.fel.cvut.cz/ontologies/record-manager/doctor";
 export const IMPERSONATOR_TYPE = "http://onto.fel.cvut.cz/ontologies/record-manager/impersonator";
+export const COMPLETE_RECORDS_TYPE = "http://onto.fel.cvut.cz/ontologies/record-manager/record-complete";
+export const REJECT_RECORDS_TYPE = "http://onto.fel.cvut.cz/ontologies/record-manager/reject-records";
+export const PUBLISH_RECORDS_TYPE = "http://onto.fel.cvut.cz/ontologies/record-manager/publish-records";
+export const DELETE_ALL_RECORDS_TYPE = "http://onto.fel.cvut.cz/ontologies/record-manager/delete-all-records";
+export const EDIT_ALL_RECORDS_TYPE = "http://onto.fel.cvut.cz/ontologies/record-manager/edit-all-records";
+export const VIEW_ALL_RECORDS_TYPE = "http://onto.fel.cvut.cz/ontologies/record-manager/view-all-records";
+export const DELETE_ORGANIZATION_RECORDS_TYPE =
+  "http://onto.fel.cvut.cz/ontologies/record-manager/delete-organization-records";
+export const EDIT_ORGANIZATION_RECORDS_TYPE =
+  "http://onto.fel.cvut.cz/ontologies/record-manager/edit-organization-records";
+export const VIEW_ORGANIZATION_RECORDS_TYPE =
+  "http://onto.fel.cvut.cz/ontologies/record-manager/view-organization-records";
+export const EDIT_USERS_TYPE = "http://onto.fel.cvut.cz/ontologies/record-manager/edit-users";
+export const IMPORT_CODELISTS_TYPE = "http://onto.fel.cvut.cz/ontologies/record-manager/import-codelists";
--- src/utils/SecurityUtils.js
+++ src/utils/SecurityUtils.js
@@ -1,7 +1,7 @@
 import { getOidcIdentityStorageKey, isUsingOidcAuth } from "./OidcUtils";
 import { sanitizeArray } from "./Utils";
 import { IMPERSONATOR_TYPE } from "../constants/Vocabulary";
-import { ROLE } from "../constants/DefaultConstants";
+import { ROLE, TYPE_ROLE } from "../constants/DefaultConstants";
 
 export function getOidcToken() {
   const identityData = sessionStorage.getItem(getOidcIdentityStorageKey());
@@ -17,10 +17,25 @@ export function clearToken() {
 }
 
 export function isAdmin(currentUser) {
-  return currentUser.role === ROLE.ADMIN;
+  return currentUser.roles ? currentUser.roles.includes(ROLE.ADMIN) : false;
+}
+
+export function hasRole(currentUser, role) {
+  return currentUser.roles ? currentUser.roles.includes(role) : false;
 }
 
 export function isImpersonator(currentUser) {
   // When using OIDC, the access token does not contain any info that the current user is being impersonated
   return !isUsingOidcAuth() && sanitizeArray(currentUser.types).indexOf(IMPERSONATOR_TYPE) !== -1;
+}
+
+export function getRoles(user) {
+  if (!user) {
+    return undefined;
+  }
+  let roles = [];
+  user.types.map((type) => {
+    TYPE_ROLE[type] && roles.push(TYPE_ROLE[type]);
+  });
+  return roles;
 }
