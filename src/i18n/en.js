854d549468f51ab9b50aa72e6efb946167268445
Daniil Palagin <daniilpalaginjob@gmail.com>
2024-09-05 19:44:28 +0200

    [#202] Fix convention. Add group input

--- src/i18n/en.js
+++ src/i18n/en.js
@@ -100,6 +100,7 @@ export default {
     "user.passwords-not-matching-tooltip": "Passwords don't match",
     "user.role": "Role",
     "user.roles": "Roles",
+    "user.group": "Group",
     "user.save-success": "User saved successfully",
     "user.save-success-with-email": "User saved successfully and informed by email.",
     "user.save-error": "Unable to save user. {error}",
