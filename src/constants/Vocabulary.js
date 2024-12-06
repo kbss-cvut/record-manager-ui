870f41d1275c08d38cb20620d67215430e1536ea
Daniil Palagin <daniilpalaginjob@gmail.com>
2024-12-06 17:08:04 +0100

    [#202] Remove unused constants

--- src/constants/Vocabulary.js
+++ src/constants/Vocabulary.js
@@ -4,22 +4,4 @@
  * Ontological vocabulary used by JSON-LD responses we get.
  */
 export const RECORD_MANAGER_URL = "http://onto.fel.cvut.cz/ontologies/record-manager";
-export const RDFS_LABEL = "http://www.w3.org/2000/01/rdf-schema#label";
-export const RDFS_COMMENT = "http://www.w3.org/2000/01/rdf-schema#comment";
-export const ADMIN_TYPE = "http://onto.fel.cvut.cz/ontologies/record-manager/administrator";
-export const DOCTOR_TYPE = "http://onto.fel.cvut.cz/ontologies/record-manager/doctor";
 export const IMPERSONATOR_TYPE = "http://onto.fel.cvut.cz/ontologies/record-manager/impersonator";
-export const COMPLETE_RECORDS_TYPE = "http://onto.fel.cvut.cz/ontologies/record-manager/complete-records";
-export const REJECT_RECORDS_TYPE = "http://onto.fel.cvut.cz/ontologies/record-manager/reject-records";
-export const PUBLISH_RECORDS_TYPE = "http://onto.fel.cvut.cz/ontologies/record-manager/publish-records";
-export const DELETE_ALL_RECORDS_TYPE = "http://onto.fel.cvut.cz/ontologies/record-manager/delete-all-records";
-export const EDIT_ALL_RECORDS_TYPE = "http://onto.fel.cvut.cz/ontologies/record-manager/edit-all-records";
-export const VIEW_ALL_RECORDS_TYPE = "http://onto.fel.cvut.cz/ontologies/record-manager/view-all-records";
-export const DELETE_ORGANIZATION_RECORDS_TYPE =
-  "http://onto.fel.cvut.cz/ontologies/record-manager/delete-organization-records";
-export const EDIT_ORGANIZATION_RECORDS_TYPE =
-  "http://onto.fel.cvut.cz/ontologies/record-manager/edit-organization-records";
-export const VIEW_ORGANIZATION_RECORDS_TYPE =
-  "http://onto.fel.cvut.cz/ontologies/record-manager/view-organization-records";
-export const EDIT_USERS_TYPE = "http://onto.fel.cvut.cz/ontologies/record-manager/edit-users";
-export const IMPORT_CODELISTS_TYPE = "http://onto.fel.cvut.cz/ontologies/record-manager/import-codelists";
