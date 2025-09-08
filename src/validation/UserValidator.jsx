48ca886f75590fa51e3aeef4095f27c013a60f25
palagdan <daniilpalaginjob@gmail.com>
2025-09-08 12:33:33 +0200

    [#284] Implement default select for Institution

--- src/validation/UserValidator.jsx
+++ src/validation/UserValidator.jsx
@@ -2,7 +2,7 @@
 
 export default class UserValidator {
   static isValid(user) {
-    return user.firstName && user.lastName && user.username && user.emailAddress && user.institution;
+    return user.firstName && user.lastName && user.username && user.emailAddress;
   }
 
   static isPasswordValid(password) {
