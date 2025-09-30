3e45ee8ff25c981643b2f0d6d5f42e45806bc45e
palagdan <daniilpalaginjob@gmail.com>
2025-09-30 12:36:08 +0200

    [#225] Update terraform configuration scripts for migration

--- deploy/keycloak-auth/keycloak-config/roles.tf
+++ deploy/keycloak-auth/keycloak-config/roles.tf
@@ -1,18 +1,25 @@
 variable "roles" {
   type = map(string)
   default = {
-    rm-delete-all-records         = ""
-    rm-edit-users                = ""
-    rm-impersonate               = ""
-    rm-edit-all-records           = ""
-    rm-view-organization-records = ""
-    rm-complete-records           = ""
-    rm-edit-organization-records = ""
-    rm-view-all-records           = ""
-    rm-delete-organization-records = ""
-    rm-publish-records            = ""
-    rm-import-codelists           = ""
-    rm-reject-records             = ""
+    read-all-records-role                 = "",
+    write-all-records-role                = "",
+    read-organization-records-role        = "",
+    write-organization-records-role       = "",
+    complete-records-role                 = "",
+    reject-records-role                   = "",
+    publish-records-role                  = "",
+    import-codelists-role                 = "",
+    comment-record-questions-role         = "",
+    read-all-users-role                   = "",
+    write-all-users-role                  = "",
+    read-organization-users-role          = "",
+    write-organization-users-role         = "",
+    read-organization-role                = "",
+    write-organization-role               = "",
+    read-all-organizations-role           = "",
+    write-all-organizations-role          = "",
+    read-action-history-role              = "",
+    read-statistics-role                  = ""
   }
 }
 
