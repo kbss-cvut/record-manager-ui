e2baece66fb3894e87548c6d4218e58e608167c9
Miroslav Blasko <blcham@gmail.com>
2025-10-02 18:48:49 +0200

    Reformat

--- deploy/keycloak-auth/keycloak-config/run.sh
+++ deploy/keycloak-auth/keycloak-config/run.sh
@@ -5,7 +5,7 @@ AUTH_SERVER_PORT="$2"
 
 until nc -z $AUTH_SERVER_HOSTNAME $AUTH_SERVER_PORT; do
   echo INFO: Waiting for keycloak authorizaion server running at $AUTH_SERVER_HOSTNAME:$AUTH_SERVER_PORT ...
-  sleep 1; 
-done 
+  sleep 1;
+done
 terraform init
 terraform apply -auto-approve
