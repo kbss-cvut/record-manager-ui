c03b976bdabb292c02350f66f52e58710591f113
palagdan <daniilpalaginjob@gmail.com>
2025-10-09 19:24:43 +0200

    [#308] Add getMessageByKey for retrieving messages in tests by id

--- tests/utils/utils.jsx
+++ tests/utils/utils.jsx
@@ -1,4 +1,5 @@
 import enLang from "../../src/i18n/en.js";
+import csLang from "../../src/i18n/cs.js";
 import { render } from "@testing-library/react";
 import { IntlProvider } from "react-intl";
 import React from "react";
@@ -9,4 +10,29 @@ export const renderWithIntl = (children) => {
       {children}
     </IntlProvider>,
   );
+};
+
+let currentTestLocale = "en";
+
+const LANGUAGE_MAP = {
+  en: enLang,
+  cs: csLang,
+};
+
+export const getCurrentLangMessages = () => {
+  const langMessages = LANGUAGE_MAP[currentTestLocale];
+  if (!langMessages) {
+    throw new Error(`No messages found for locale: ${currentTestLocale}`);
+  }
+  return langMessages.messages || {};
+};
+
+export const getMessageByKey = (key) => {
+  const messages = getCurrentLangMessages();
+  const message = messages[key];
+  if (!message) {
+    console.warn(`Message key "${key}" not found for locale: ${currentTestLocale}`);
+    return key;
+  }
+  return message;
 };
