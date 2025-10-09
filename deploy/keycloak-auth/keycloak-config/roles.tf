fc3a8c05e413656413d1f3578e8e564ac233f18f
palagdan <daniilpalaginjob@gmail.com>
2025-10-09 16:58:54 +0200

    Add description about roles to keycloak instance

--- deploy/keycloak-auth/keycloak-config/roles.tf
+++ deploy/keycloak-auth/keycloak-config/roles.tf
@@ -1,27 +1,66 @@
 variable "roles" {
   type = map(string)
   default = {
-    read-all-records-role                 = "",
-    write-all-records-role                = "",
-    read-organization-records-role        = "",
-    write-organization-records-role       = "",
-    complete-records-role                 = "",
-    reject-records-role                   = "",
-    publish-records-role                  = "",
-    import-codelists-role                 = "",
-    comment-record-questions-role         = "",
-    read-all-users-role                   = "",
-    write-all-users-role                  = "",
-    read-organization-users-role          = "",
-    write-organization-users-role         = "",
-    read-organization-role                = "",
-    write-organization-role               = "",
-    read-all-organizations-role           = "",
-    write-all-organizations-role          = "",
-    read-action-history-role              = "",
-    read-statistics-role                  = ""
+    read-all-records-role = <<EOT
+Allows users to read all records in the system, regardless of the organization they belong to.
+EOT
+    write-all-records-role = <<EOT
+Allows users to create, update, or delete any record in the system, regardless of organization.
+EOT
+    read-organization-records-role = <<EOT
+Allows users to read records belonging to their own organization only.
+EOT
+    write-organization-records-role = <<EOT
+Allows users to create, update, or delete records within their own organization only.
+EOT
+    complete-records-role = <<EOT
+Allows users to mark records as complete.
+EOT
+    reject-records-role = <<EOT
+Allows users to reject records.
+EOT
+    publish-records-role = <<EOT
+Allows users to publish records.
+EOT
+    import-codelists-role = <<EOT
+Allows users to import codelists into the system.
+EOT
+    comment-record-questions-role = <<EOT
+Allows users to add comments to questions in records.
+EOT
+    read-all-users-role = <<EOT
+Allows users to view the details of all users in the system, regardless of their organization.
+EOT
+    write-all-users-role = <<EOT
+Allows users to create, update, or delete any user account in the system, including modifying user attributes.
+EOT
+    read-organization-users-role = <<EOT
+Allows users to view the details of users within their own organization only.
+EOT
+    write-organization-users-role = <<EOT
+Allows users to create, update, or delete user accounts within their own organization only.
+EOT
+    read-organization-role = <<EOT
+Allows users to view details about the user’s own organization, such as its name or email.
+EOT
+    write-organization-role = <<EOT
+Allows users to modify their own organization’s details, such as updating its name or email.
+EOT
+    read-all-organizations-role = <<EOT
+Allows users to view details of all organizations in the system.
+EOT
+    write-all-organizations-role = <<EOT
+Allows users to create, update, or delete any organization in the system.
+EOT
+    read-action-history-role = <<EOT
+Allows users to view the history of operations performed in the system.
+EOT
+    read-statistics-role = <<EOT
+Allows users to view statistical reports about the system’s data.
+EOT
   }
 }
+
 
 resource "keycloak_role" "realm_roles" {
   for_each  = var.roles
