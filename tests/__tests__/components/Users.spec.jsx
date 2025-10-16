fce6e8e02367099037394fcd32f9d6e61be50c9c
palagdan <daniilpalaginjob@gmail.com>
2025-10-16 16:40:45 +0200

    [#308] test: rewrite Users tests using Testing Library

--- tests/__tests__/components/Users.spec.jsx
+++ tests/__tests__/components/Users.spec.jsx
@@ -1,66 +1,68 @@
-import React from "react";
 import Users from "../../../src/components/user/Users";
 import { ACTION_STATUS } from "../../../src/constants/DefaultConstants";
 import { describe, expect, it, vi, beforeEach } from "vitest";
-import { renderWithIntl } from "../../utils/utils.jsx";
+import { getMessageByKey, renderWithIntl } from "../../utils/utils.jsx";
 import { screen, fireEvent } from "@testing-library/react";
 import { isUsingOidcAuth } from "../../../src/utils/OidcUtils.js";
 import "@testing-library/jest-dom";
+import React from "react";
 
 vi.mock("../../../src/utils/OidcUtils", () => ({
   isUsingOidcAuth: vi.fn(() => false),
 }));
 
-const users = [
-  {
-    username: "testman1",
+vi.mock("../../../src/components/user/UserTable", () => ({
+  default: () => <div data-testid="record-table">Users Table</div>,
+}));
+
+const defaultProps = {
+  handlers: {
+    onEdit: vi.fn(),
+    onCreate: vi.fn(),
+    onDelete: vi.fn(),
   },
-  {
-    username: "testman2",
+  usersLoaded: {
+    status: ACTION_STATUS.SUCCESS,
+    users: [
+      {
+        username: "batman",
+      },
+      {
+        username: "joker",
+      },
+    ],
   },
-];
-
-const handlers = {
-  onEdit: vi.fn(),
-  onCreate: vi.fn(),
-  onDelete: vi.fn(),
+  usersLoadedEmpty: {
+    status: ACTION_STATUS.SUCCESS,
+    users: [],
+  },
 };
 
-const usersLoaded = {
-  status: ACTION_STATUS.SUCCESS,
-  users,
+const renderComponent = (props) => {
+  return renderWithIntl(<Users {...defaultProps} {...props} />);
 };
-
-const usersLoadedEmpty = {
-  status: ACTION_STATUS.SUCCESS,
-  users: [],
-};
-
-beforeEach(() => {
-  vi.clearAllMocks();
-});
 
 describe("Users", function () {
-  const renderComponent = (usersLoaded = users, handlersList = handlers, overrides = {}) => {
-    return renderWithIntl(<Users usersLoaded={usersLoaded} handlers={handlersList} {...overrides} />);
-  };
-
   beforeEach(() => {
     vi.clearAllMocks();
   });
 
-  it("renders a 'Create user' button when Internal Authorization configured", () => {
+  it("renders title with users table", () => {
     renderComponent();
-
-    const button = screen.getByRole("button", { name: /Create user/i });
-    fireEvent.click(button);
-
-    expect(handlers.onCreate).toHaveBeenCalledTimes(1);
+    expect(screen.getByText(getMessageByKey("users.panel-title"))).toBeInTheDocument();
+    expect(screen.getByTestId("record-table")).toBeInTheDocument();
   });
 
-  it("hides `Create user` button when using OIDC auth", () => {
-    vi.mocked(isUsingOidcAuth).mockReturnValue(true);
+  it("renders a 'Create user' button when Internal Authorization configured", () => {
     renderComponent();
-    expect(screen.queryByRole("button", { name: /Create user/i })).not.toBeInTheDocument();
+    const button = screen.getByRole("button", { name: getMessageByKey("users.create-user") });
+    fireEvent.click(button);
+    expect(defaultProps.handlers.onCreate).toHaveBeenCalledTimes(1);
+  });
+
+  it("does not render `Create user` button when using OIDC auth", () => {
+    isUsingOidcAuth.mockReturnValue(true);
+    renderComponent();
+    expect(screen.queryByRole("button", { name: getMessageByKey("users.create-user") })).not.toBeInTheDocument();
   });
 });
